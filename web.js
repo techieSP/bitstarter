96ec166326adfb5740c271776027eb7fabd4880b
Andrés RS <andres.r.s@gmail.com>
2013-07-10 22:29:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
+var fs=require('fs');
+var textBuffer=fs.readFileSync("index.html");
+var text=textBuffer.toString();
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
